66eb0ea84bec737afa65c80779a1b79e54ada22a
rodion <rodionaaafgh@gmail.com>
2019-03-15 10:43:09 +0300

    fix: watcher

--- src/watcher.js
+++ src/watcher.js
@@ -1,4 +1,5 @@
 const AWS = require('aws-sdk')
+const Sentry = require('@sentry/node')
 
 const { FEATURE_STATUS } = require('./constants')
 const notify = require('./utils/notify')
@@ -7,20 +8,29 @@ const usersTable = require('../db/users')
 
 module.exports = {
   processRecord: async ({ dynamodb: { Keys, NewImage, OldImage } }) => {
-    const [before, after] = [OldImage, NewImage]
-      .map(item => (item ? AWS.DynamoDB.Converter.unmarshall(item) : undefined))
-      
-    if (!before && after.status === FEATURE_STATUS.WAITING_FOR_CONFIRMATION)
-    {
-      await notify.newFeatureSubmit(after)
-    }
-    if (before && after) {
-      const { email } = await usersTable.get(ownerId, ['email'])
-      if (before.status === FEATURE_STATUS.WAITING_FOR_CONFIRMATION && after.status === FEATURE_STATUS.IN_QUEUE) {
-        await emailUtils.featureMovedIntoProgress(email, after.name)
-      } else if (before.status === FEATURE_STATUS.IN_QUEUE && after.status === FEATURE_STATUS.DONE) {
-        await emailUtils.featureDone(email, after.name)
+    try {
+      const [before, after] = [OldImage, NewImage]
+        .map(item => (item ? AWS.DynamoDB.Converter.unmarshall(item) : undefined))
+        
+      if (!before && after.status === FEATURE_STATUS.WAITING_FOR_CONFIRMATION)
+      {
+        await notify.newFeatureSubmit(after)
       }
+      if (before && after) {
+        const { email } = await usersTable.get(after.ownerId, ['email'])
+        if (before.status === FEATURE_STATUS.WAITING_FOR_CONFIRMATION && after.status === FEATURE_STATUS.IN_QUEUE) {
+          await emailUtils.featureMovedIntoProgress(email, after.name)
+        } else if (before.status === FEATURE_STATUS.IN_QUEUE && after.status === FEATURE_STATUS.DONE) {
+          await emailUtils.featureDone(email, after.name)
+        }
+      }
+    } catch (err) {
+      Sentry.configureScope(scope => {
+        Object.entries(error).map(([key, value]) => scope.setExtra(key, value))
+      })
+      Sentry.captureException(
+        'Watcher Fail'
+      )
     }
   }
 }
